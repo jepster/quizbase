e8a10cbb1e81ca16c0a1cf43697b3755a0bb3cde
Peter Majmesku <peter.majmesku@adesso.de>
2024-10-14 20:37:46 +0200

    Could successfully fetch a letsencrypt certificate

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:63342', 'http://localhost:3000'],
+    origin: ['http://localhost:63342', 'http://localhost:3000', 'http://104.248.132.247:3000'],
     methods: ['GET', 'POST'],
     credentials: true,
   });
