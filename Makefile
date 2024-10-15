089c245cb9669247549a7a02c078da0aa891eabb
Peter Majmesku <peter.majmesku@adesso.de>
2024-10-15 20:45:58 +0200

    Finally I found a docker compose configuration, which does work

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 deploy:
-	rsync -e "ssh -o StrictHostKeyChecking=no" -rltgoD --no-perms --no-owner --no-group --no-times --progress --delete -v --stats --progress ./ root@104.248.132.247:/root/app
+	rsync -e "ssh -o StrictHostKeyChecking=no" -rltgoD --no-perms --no-owner --no-group --no-times --progress --delete -v --stats --progress ./ --exclude=server/node_modules/ root@104.248.132.247:/root/app
 
 ssh:
 	ssh root@104.248.132.247
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:63342', 'http://localhost:3000', 'http://104.248.132.247:3000'],
+    origin: [
+      'http://backend',
+      'http://0.0.0.0',
+      'https://mobilefish.de',
+      'http://localhost:63342',
+      'http://localhost:3000',
+      'http://104.248.132.247:3000',
+    ],
     methods: ['GET', 'POST'],
     credentials: true,
   });
