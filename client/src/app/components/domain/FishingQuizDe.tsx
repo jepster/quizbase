31a63991628a7b8fcacb3176fac45cb861dfa533
Peter Majmesku <peter.majmesku@gmail.com>
2025-02-09 15:32:04 +0100

    Configured matomo

--- client/src/app/components/domain/FishingQuizDe.tsx
+++ client/src/app/components/domain/FishingQuizDe.tsx
@@ -22,6 +22,20 @@ export default function FishingQuizDe() {
     if (cookieConsent === 'accepted') {
       setShowCookieBanner(false);
     }
+
+    // @ts-expect-error Matomo snippet code.
+    const _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      const u="//fishingquiz.de/matomo/";
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '1']);
+      const d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      // @ts-expect-error Matomo snippet code.
+      g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
   }, []);
 
   const resetGame = () => {
